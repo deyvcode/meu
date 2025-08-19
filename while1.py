4e8bd915b778ab4c100632cdbe67e14509271533
deyvcode <deyvidwilliansalesdearaujo@gmail.com>
2025-08-19 17:47:33 -0300

    Add files via upload

---
+++ while1.py
@@ -0,0 +1,9 @@
+num = 11
+
+
+
+while num>1:
+    num=num-1
+    print(num)
+
+print('FOGOOOO')
---
+++ while2.py
@@ -0,0 +1,9 @@
+fale=int(input('Digite um numero entre 0 e 10: '))
+
+senha='0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10'
+
+while fale<0 or fale>10:
+    fale=int(input('Digite novamente: '))
+
+
+print('Nota enviada')
