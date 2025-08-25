ed04e2bc46202d59825569c739ccfb403271d91f
deyvcode <deyvidwilliansalesdearaujo@gmail.com>
2025-08-25 17:51:46 -0300

    Add files via upload

---
+++ função.py
@@ -0,0 +1,9 @@
+def soma (n1,n2):
+ return n1+n2
+
+
+
+def resultado (soma,n3):
+    return(soma*n3)
+
+print (resultado(soma(2,4),3))
---
+++ função2.py
@@ -0,0 +1,5 @@
+def print (nome: 
+
+def print (idade: )
+
+return (nome,idade)
